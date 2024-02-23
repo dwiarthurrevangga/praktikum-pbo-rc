873c26329e2e7cd47ac1ffc0d888a929b582fb74
dwiarthurrevangga <143766362+dwiarthurrevangga@users.noreply.github.com>
2024-02-23 12:37:36 +0700

    Create 122140144_DWI ARTHUR REVANGGA_Pset{3}.py

---
+++ week0-pbo-rc/122140144_DWI ARTHUR REVANGGA_Pset{3}.py
@@ -0,0 +1,6 @@
+file = open("Me.txt", "w")
+file.write("Nama : Dwi Arthur Revangga\n")
+file.write("NIM : 122140144\n")
+file.write("Resolusi Saya di tahun ini : punya duit")
+
+print("Data telah disimpan ke dalam Me.txt")
